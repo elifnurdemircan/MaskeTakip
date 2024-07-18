dbb691c0d4ba96b80cf8863e376f35e1f6e32ba3
demir <demir@ELIFNUR>
2024-07-18 18:35:43 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,121 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using Business.Concrete;
+using Entities1.Concrete;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        SelamVer(isim: " Engin ");
+        SelamVer(isim: " Ahmet ");
+        SelamVer(isim: " Ayşe ");
+        SelamVer();  
+
+        int sonuc = Topla(6, 58);
+
+        //Diziler / Arrays
+
+        string ogrenci1 = "Engin";
+        string ogrenci2 = "Kerem";
+        string ogrenci3 = "Berkay";
+
+        //Console.WriteLine(ogrenci1);
+        //Console.WriteLine(ogrenci2);
+        //Console.WriteLine(ogrenci3);
+
+
+        string[] ogrenciler = new string[3];
+        ogrenciler[0] = "Engin";
+        ogrenciler[1] = "Kerem";
+        ogrenciler[2] = "Berkay";
+
+        ogrenciler = new string[4];
+        ogrenciler[3] = "İlker";
+
+        for (int i = 0; i < ogrenciler.Length; i++)
+        {
+            Console.WriteLine(ogrenciler[i]);
+        }
+
+        string[] sehirler1 = new[] { "Ankara", "İstanbul", "İzmir" };
+        string[] sehirler2 = new[] { "Bursa", "Antalya", "Diyarbakır" };
+
+        sehirler2 = sehirler1;
+        sehirler1[0] = "Adana";
+        Console.WriteLine(sehirler2[0]);
+
+        Person person1 = new Person();
+        person1.FirstName = "ENGİN";
+        person1.LastName = "DEMİROĞ";
+        person1.DateOfBirthYear = 1985;
+        person1.NationalIdentity = 123;
+
+
+        Person person2 = new Person();
+        person2.FirstName = "Murat";
+
+        foreach (string sehir in sehirler1)
+        {
+            Console.WriteLine(sehir);
+        }
+
+        List<string> yeniSehirler1 = new List<string> { "Ankara1", "İstanbul1", "İzmir1" };
+        yeniSehirler1.Add(item: "Adana 1");
+        foreach (var sehir in yeniSehirler1)
+        {
+            Console.WriteLine(sehir);
+        }
+
+        PttManager pttManager = new PttManager(new PersonManager());
+        pttManager.GiveMask(person1);
+
+        Console.ReadLine();
+
+        //resharper
+        static void SelamVer(string isim = "isimsiz")
+        {
+            Console.WriteLine("Merhaba" + isim);
+        }
+
+        static int Topla(int sayi1 = 5, int sayi2 = 10)
+        {
+            int sonuc = sayi1 + sayi2;
+            Console.WriteLine("Toplam : " + sonuc.ToString());
+            return sonuc;
+        }
+
+        static void Degiskenler()
+        {
+            string mesaj = "Merhaba";
+            double tutar = 100000; //dbden gelir
+            int sayi = 100;
+            bool girisYapmisMi = false;
+
+            string ad = "Engin";
+            string soyad = "Demiroğ";
+            string dogumYili = "1985";
+            string tcNo = "12345678910";
+
+
+
+            Console.WriteLine(100000 * 1.18);
+
+            Console.WriteLine(100000 * 1.18);
+        }
+    }
+
+
+
+
+    public class Vatandas
+    {
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public int DogumYili { get; set; }
+        public long TcNo { get; set; }
+
+    }
+}
+
+
---
+++ Workaround/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Workaround
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            SelamVer(isim:"Engin");
+            SelamVer(isim:"Ahmet");
+            SelamVer(isim:"Ayşe");
+            SelamVer();
+
+            int sonuc = Topla(6,58);
+
+            //Diziler / Arrays
+
+            string ogrenci1 = "Engin";
+            string ogrenci2 = "Kerem";
+            string ogrenci3 = "Berkay";
+
+            Console.WriteLine(ogrenci1);
+            Console.WriteLine(ogrenci2);
+            Console.WriteLine(ogrenci3);
+
+
+            string[] ogrenciler = new string[3];
+            ogrenciler[0] = "Engin";
+            ogrenciler[1] = "Kerem";
+            ogrenciler[2] = "Berkay";
+
+            ogrenciler = new string[4];
+            ogrenciler[3] = "İlker";
+
+            for(int i = 0; i < ogrenciler.Length; i++)
+            {
+                Console.WriteLine(ogrenciler[i]);
+            }
+
+            string[] sehirler1 = new[] { "Ankara", "İstanbul", "İzmir" };
+            string[] sehirler2 = new[] { "Bursa", "Antalya", "Diyarbakır" };
+
+            sehirler2 = sehirler1;
+            sehirler1[0] = "Adana";
+            Console.Write(sehirler2[0]);
+
+            Person person1 = new Person();
+            person1.FirstName = "Engin";
+
+            Person person2 = new Person();
+            person2.FirstName = "Murat";
+
+            foreach (string s in sehirler1)
+            {
+                Console.WriteLine(sehir);
+            }
+
+            List<string> yeniSehirler1 = new List<string>("Ankara", "İstanbul", "İzmir");
+            yeniSehirler1.Add(item: "Adana 1");
+            foreach (var sehir in yeniSehirler1)
+            {
+                Console.Writeline(sehir);
+            }
+
+            Console.ReadLine();
+        }
+
+
+        //resharper
+        static void SelamVer(string isim="isimsiz")
+        {
+            Console.WriteLine("Merhaba" + isim);
+        }
+
+        static int Topla(int sayi1 = 5, int sayi2 = 10)
+        {
+            int sonuc = sayi1 + sayi2;
+            Console.WriteLine("Toplam : "+ sonuc.ToString());
+            return sonuc;
+        }
+
+        private static void Degiskenler()
+        {
+            string mesaj = "Merhaba";
+            double tutar = 100000; //dbden gelir
+            int sayi = 100;
+            bool girisYapmisMi = false;
+
+           string ad = "Engin";
+           string soyad = "Demiroğ";
+           string dogumYili = "1985";
+           string tcNo = "12345678910";
+
+
+
+            Console.WriteLine(100000 * 1.18);
+
+            Console.WriteLine(100000 * 1.18);
+        }
+    }
+   public class Vatandas
+    {
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public int DogumYili { get; set; }
+        public long TcNo { get; set; }
+
+
+    }
+}
+
